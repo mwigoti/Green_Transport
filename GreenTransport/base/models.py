a3f8380ed1f5be66c2f06670da24fa6d94193e63
mwigoti <henrymwoha02@gmail.com>
2024-03-25 14:14:59 -0500

    manage

--- GreenTransport/base/models.py
+++ GreenTransport/base/models.py
@@ -6,4 +6,10 @@ from email.policy import default
 class Car(models.Model):
     car_id=models.IntegerField(default=0)
     car_name=models.CharField(max_length=30,default="")
-    car_desc=models.CharField(max_length=300)
+    car_desc=models.CharField(max_length=300, default="")
+    price=models.IntegerField(default=0)
+    image=models.ImageField(upload_to="car/images", default="")
+
+    def __str__(self):
+        return self.car_name
+    
